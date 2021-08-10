6d6387c2a6343ea76844421c21bf9f22e1925be6
ktr92 <kudashevtim@gmail.com>
2021-08-10 14:46:27 +0300

    start

--- js/main.js
+++ js/main.js
@@ -1,5 +1,37 @@
 $(document).ready(function () {
 
+	$('.menubutton').click(function (e) {
+    $('.mainmenu').toggle('slide');
+  });
+
+  $('.searchbutton').click(function (e) {
+    $('.search').slideToggle();
+  });
+  $('.closesearch').click(function (e) {
+    $('.search').slideToggle();
+  });
+
+  
+ $('.filter__title').click(function (e) {
+    $('.filter__form').slideToggle();
+  });
+
+
+  $(document).mouseup(function(e) 
+{
+    var container = $(".search");
+    var ext = $(".searchbutton");
+
+    // if the target of the click isn't the container nor a descendant of the container
+    if (!ext.is(e.target) &&  !container.is(e.target) && ext.has(e.target).length === 0) 
+    {
+        container.hide();
+    }
+});
+
+
+
+
 	try {
 		$('.mainslider').slick({
 			infinite: true,
